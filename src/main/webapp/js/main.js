abce39034be890ff2ac37cef633981cefeab7d5e
olegcntu <oleg.krasavcik.41@gmail.com>
2022-08-14 13:53:52 +0300

    with work slider

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,5 +1,12 @@
 $(function (){
 
-    $('.slider_inner').slick()
+    $('.slider_inner').slick(
+        {
+            nextArrow: '<button type="button"class="slick-btn slick-next"></button>',
+            prevArrow: '<button type="button"class="slick-btn slick-prev"></button>',
+            infinite: false
+
+        }
+    )
 
 });
