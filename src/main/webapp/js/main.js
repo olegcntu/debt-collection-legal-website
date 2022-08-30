f9d3a7c8f75b34972ea3ff6268e401c21b8f289d
olegcntu <oleg.krasavcik.41@gmail.com>
2022-08-30 23:05:53 +0300

    work with section news

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,6 +1,6 @@
 $(function (){
 
-    $('.slider_inner').slick(
+    $('.slider_inner, .news_slider-inner').slick(
         {
             nextArrow: '<button type="button"class="slick-btn slick-next"></button>',
             prevArrow: '<button type="button"class="slick-btn slick-prev"></button>',
@@ -10,4 +10,5 @@ $(function (){
     )
 
     $('select').styler();
+
 });
